c4a082ed7a90ca6be6246a344948ff8b941fcf1b
lxy <lxy2011w@163.com>
2022-02-08 00:25:33 +0800

    feat: Gateway转发admin请求；跨域

---
+++ mall-gateway/src/main/java/cn/xylvvv/gulimall/gateway/config/MallCorsConfiguration.java
@@ -0,0 +1,24 @@
+package cn.xylvvv.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class MallCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
