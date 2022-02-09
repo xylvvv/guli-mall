fa9ce96baf598cc5a37e06dfed91cc0b1406e2df
lxy <lxy2011w@163.com>
2022-02-10 02:36:10 +0800

    feat: 阿里云OSS服务；Nacos配置中心

--- mall-product/src/main/java/cn/xylvvv/gulimall/product/controller/CategoryController.java
+++ mall-product/src/main/java/cn/xylvvv/gulimall/product/controller/CategoryController.java
@@ -59,7 +59,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -78,6 +78,13 @@ public class CategoryController {
     @RequestMapping("/update")
     public R update(@RequestBody CategoryEntity category){
 		categoryService.updateById(category);
+
+        return R.ok();
+    }
+
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
 
         return R.ok();
     }
