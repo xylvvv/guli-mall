49de0de12efce224cc97bb66bc48d93bd1cfd1a1
lxy <lxy2011w@163.com>
2022-03-09 11:13:05 +0800

    feat: 认证服务-短信验证码

---
+++ mall-third-party/src/main/java/cn/xylvvv/thirdparty/controller/SmsSendController.java
@@ -0,0 +1,31 @@
+package cn.xylvvv.thirdparty.controller;
+
+import cn.xylvvv.common.utils.R;
+import cn.xylvvv.thirdparty.component.SmsComponent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "/sms")
+public class SmsSendController {
+
+    @Resource
+    private SmsComponent smsComponent;
+
+    /**
+     * 提供给别的服务进行调用
+     * @param phone
+     * @param code
+     * @return
+     */
+    @GetMapping(value = "/sendCode")
+    public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code) {
+        //发送验证码
+        smsComponent.sendCode(phone,code);
+        return R.ok();
+    }
+}
